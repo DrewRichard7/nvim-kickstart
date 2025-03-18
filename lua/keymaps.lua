598813dd5c1c46e6c526ea741aac2df557115c87
Andrew Richard <acrichard07@gmail.com>
2025-03-18 12:15:52 -0700

    trying to rearrange file structure to get which-key to be recognized

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -50,26 +50,212 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 -- my additional keymaps
 
 -- my personal keymaps
-local keymap = vim.keymap
-keymap.set('i', 'jk', '<ESC>', { desc = 'Exit insert mode with jk' })
+vim.keymap.set('i', 'jk', '<ESC>', { desc = 'Exit insert mode with jk' })
 
-keymap.set('n', '<leader>to', '<cmd>tabnew<CR>', { desc = 'Open new tab' }) -- open new tab
-keymap.set('n', '<leader>tw', '<cmd>tabclose<CR>', { desc = 'Close current tab' }) -- close current tab
-keymap.set('n', '<leader>tn', '<cmd>tabn<CR>', { desc = 'Go to next tab' }) --  go to next tab
-keymap.set('n', '<leader>tp', '<cmd>tabp<CR>', { desc = 'Go to previous tab' }) --  go to previous tab
-keymap.set('n', '<leader>tf', '<cmd>tabnew %<CR>', { desc = 'Open current buffer in new tab' }) --  move current buffer to new tab
+vim.keymap.set('n', '<leader>to', '<cmd>tabnew<CR>', { desc = 'Open new tab' }) -- open new tab
+vim.keymap.set('n', '<leader>tw', '<cmd>tabclose<CR>', { desc = 'Close current tab' }) -- close current tab
+vim.keymap.set('n', '<leader>tn', '<cmd>tabn<CR>', { desc = 'Go to next tab' }) --  go to next tab
+vim.keymap.set('n', '<leader>tp', '<cmd>tabp<CR>', { desc = 'Go to previous tab' }) --  go to previous tab
+vim.keymap.set('n', '<leader>tf', '<cmd>tabnew %<CR>', { desc = 'Open current buffer in new tab' }) --  move current buffer to new tab
 
 -- center cursor on half page scroll
-keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'keeps half page down scroll centered on screen' })
-keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'keeps half page up scroll centered on screen' })
+vim.keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'keeps half page down scroll centered on screen' })
+vim.keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'keeps half page up scroll centered on screen' })
 
 -- center cursor remap
-keymap.set('n', 'j', 'jzz', { desc = 'scroll down and center cursor' })
-keymap.set('n', 'k', 'kzz', { desc = 'scroll up and center cursor' })
+vim.keymap.set('n', 'j', 'jzz', { desc = 'scroll down and center cursor' })
+vim.keymap.set('n', 'k', 'kzz', { desc = 'scroll up and center cursor' })
 
 -- visual mode keymaps
-keymap.set('v', 'J', ":m '>+1<CR>gv=gv", { desc = 'move block down' }) -- moves highlighted code down by line
-keymap.set('v', 'K', ":m '<-2<CR>gv=gv", { desc = 'move block up' }) -- moves highlighted code up
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", { desc = 'move block down' }) -- moves highlighted code down by line
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv", { desc = 'move block up' }) -- moves highlighted code up
 
 -- pipe operator |>
-keymap.set('i', '<C-m>', '|>', { desc = 'insert pip operator' })
+vim.keymap.set('i', '<C-m>', '|>', { desc = 'insert pip operator' })
+
+P = vim.print
+
+vim.g['quarto_is_r_mode'] = nil
+vim.g['reticulate_running'] = false
+
+local nmap = function(key, effect)
+  vim.keymap.set('n', key, effect, { silent = true, noremap = true })
+end
+
+local vmap = function(key, effect)
+  vim.keymap.set('v', key, effect, { silent = true, noremap = true })
+end
+
+local imap = function(key, effect)
+  vim.keymap.set('i', key, effect, { silent = true, noremap = true })
+end
+
+local cmap = function(key, effect)
+  vim.keymap.set('c', key, effect, { silent = true, noremap = true })
+end
+
+-- move in command line
+cmap('<C-a>', '<Home>')
+
+-- save with ctrl+s
+imap('<C-s>', '<esc>:update<cr><esc>')
+nmap('<C-s>', '<cmd>:update<cr><esc>')
+
+-- Move between windows using <ctrl> direction
+nmap('<C-j>', '<C-W>j')
+nmap('<C-k>', '<C-W>k')
+nmap('<C-h>', '<C-W>h')
+nmap('<C-l>', '<C-W>l')
+
+-- Resize window using <shift> arrow keys
+nmap('<S-Up>', '<cmd>resize +2<CR>')
+nmap('<S-Down>', '<cmd>resize -2<CR>')
+nmap('<S-Left>', '<cmd>vertical resize -2<CR>')
+nmap('<S-Right>', '<cmd>vertical resize +2<CR>')
+
+-- Add undo break-points
+imap(',', ',<c-g>u')
+imap('.', '.<c-g>u')
+imap(';', ';<c-g>u')
+
+nmap('Q', '<Nop>')
+
+--- Send code to terminal with vim-slime
+--- If an R terminal has been opend, this is in r_mode
+--- and will handle python code via reticulate when sent
+--- from a python chunk.
+--- TODO: incorpoarate this into quarto-nvim plugin
+--- such that QuartoRun functions get the same capabilities
+--- TODO: figure out bracketed paste for reticulate python repl.
+local function send_cell()
+  if vim.b['quarto_is_r_mode'] == nil then
+    vim.fn['slime#send_cell']()
+    return
+  end
+  if vim.b['quarto_is_r_mode'] == true then
+    vim.g.slime_python_ipython = 0
+    local is_python = require('otter.tools.functions').is_otter_language_context 'python'
+    if is_python and not vim.b['reticulate_running'] then
+      vim.fn['slime#send']('reticulate::repl_python()' .. '\r')
+      vim.b['reticulate_running'] = true
+    end
+    if not is_python and vim.b['reticulate_running'] then
+      vim.fn['slime#send']('exit' .. '\r')
+      vim.b['reticulate_running'] = false
+    end
+    vim.fn['slime#send_cell']()
+  end
+end
+
+--- Send code to terminal with vim-slime
+--- If an R terminal has been opend, this is in r_mode
+--- and will handle python code via reticulate when sent
+--- from a python chunk.
+local slime_send_region_cmd = ':<C-u>call slime#send_op(visualmode(), 1)<CR>'
+slime_send_region_cmd = vim.api.nvim_replace_termcodes(slime_send_region_cmd, true, false, true)
+local function send_region()
+  -- if filetyps is not quarto, just send_region
+  if vim.bo.filetype ~= 'quarto' or vim.b['quarto_is_r_mode'] == nil then
+    vim.cmd('normal' .. slime_send_region_cmd)
+    return
+  end
+  if vim.b['quarto_is_r_mode'] == true then
+    vim.g.slime_python_ipython = 0
+    local is_python = require('otter.tools.functions').is_otter_language_context 'python'
+    if is_python and not vim.b['reticulate_running'] then
+      vim.fn['slime#send']('reticulate::repl_python()' .. '\r')
+      vim.b['reticulate_running'] = true
+    end
+    if not is_python and vim.b['reticulate_running'] then
+      vim.fn['slime#send']('exit' .. '\r')
+      vim.b['reticulate_running'] = false
+    end
+    vim.cmd('normal' .. slime_send_region_cmd)
+  end
+end
+
+-- send code with ctrl+Enter
+-- just like in e.g. RStudio
+-- needs kitty (or other terminal) config:
+-- map shift+enter send_text all \x1b[13;2u
+-- map ctrl+enter send_text all \x1b[13;5u
+nmap('<c-cr>', send_cell)
+nmap('<s-cr>', send_cell)
+imap('<c-cr>', send_cell)
+imap('<s-cr>', send_cell)
+
+--- Show R dataframe in the browser
+-- might not use what you think should be your default web browser
+-- because it is a plain html file, not a link
+-- see https://askubuntu.com/a/864698 for places to look for
+local function show_r_table()
+  local node = vim.treesitter.get_node { ignore_injections = false }
+  assert(node, 'no symbol found under cursor')
+  local text = vim.treesitter.get_node_text(node, 0)
+  local cmd = [[call slime#send("DT::datatable(]] .. text .. [[)" . "\r")]]
+  vim.cmd(cmd)
+end
+
+-- keep selection after indent/dedent
+vmap('>', '>gv')
+vmap('<', '<gv')
+
+-- center after search and jumps
+nmap('n', 'nzz')
+nmap('<c-d>', '<c-d>zz')
+nmap('<c-u>', '<c-u>zz')
+
+-- move between splits and tabs
+nmap('<c-h>', '<c-w>h')
+nmap('<c-l>', '<c-w>l')
+nmap('<c-j>', '<c-w>j')
+nmap('<c-k>', '<c-w>k')
+nmap('H', '<cmd>tabprevious<cr>')
+nmap('L', '<cmd>tabnext<cr>')
+
+local function toggle_light_dark_theme()
+  if vim.o.background == 'light' then
+    vim.o.background = 'dark'
+  else
+    vim.o.background = 'light'
+  end
+end
+
+local is_code_chunk = function()
+  local current, _ = require('otter.keeper').get_current_language_context()
+  if current then
+    return true
+  else
+    return false
+  end
+end
+
+--- Insert code chunk of given language
+--- Splits current chunk if already within a chunk
+--- @param lang string
+local insert_code_chunk = function(lang)
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<esc>', true, false, true), 'n', true)
+  local keys
+  if is_code_chunk() then
+    keys = [[o```<cr><cr>```{]] .. lang .. [[}<esc>o]]
+  else
+    keys = [[o```{]] .. lang .. [[}<cr>```<esc>O]]
+  end
+  keys = vim.api.nvim_replace_termcodes(keys, true, false, true)
+  vim.api.nvim_feedkeys(keys, 'n', false)
+end
+
+local insert_r_chunk = function()
+  insert_code_chunk 'r'
+end
+
+local insert_py_chunk = function()
+  insert_code_chunk 'python'
+end
+
+-- require('which-key').add({
+--   { '<cm-i>', insert_py_chunk, desc = 'python code chunk' },
+--   { '<esc>', '<cmd>noh<cr>', desc = 'remove search highlight' },
+--   { '<m-I>', insert_py_chunk, desc = 'python code chunk' },
+--   { '<m-i>', insert_r_chunk, desc = 'r code chunk' },
+-- }, { mode = 'n', silent = true })
--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -211,8 +211,8 @@ return {
       local servers = {
         -- clangd = {},
         -- gopls = {},
-        -- pyright = {},
-        -- rust_analyzer = {},
+        pyright = {},
+        rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
@@ -232,7 +232,10 @@ return {
                 callSnippet = 'Replace',
               },
               -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-              -- diagnostics = { disable = { 'missing-fields' } },
+              diagnostics = {
+                disable = { 'missing-fields' },
+                globals = { 'vim', 'require' },
+              },
             },
           },
         },
