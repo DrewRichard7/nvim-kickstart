f3b49c579405335791fbbf0207d91b48d8036dad
Andrew Richard <acrichard07@gmail.com>
2025-03-19 08:32:55 -0700

    put color themes in new single file, added lualine

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -37,7 +37,7 @@ require('lazy').setup({
 
   -- require 'plugins/tokyonight',
 
-  require 'plugins/catppuccin',
+  require 'plugins/colorscheme',
 
   require 'plugins/todo-comments',
 
@@ -60,6 +60,8 @@ require('lazy').setup({
   require 'plugins/snacks',
 
   require 'plugins.bufferline',
+
+  require 'plugins.lualine',
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
